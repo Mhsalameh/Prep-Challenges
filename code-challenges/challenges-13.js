1d92ac4f15fed62a8656e45df9ed3fa9a77ef056
Mhsalameh <mohsalameh1@gmail.com>
2022-02-01 20:13:11 +0200

    exercise13 is all done

---
+++ code-challenges/challenges-13.js
@@ -0,0 +1,126 @@
+'use strict';
+
+// Important Note: 
+// Kindly use reduce instead of for in all of your solutions
+
+// Resource:
+// Reduce: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/Reduce
+
+// 1) ---------------------
+// 
+//  Given an array of objects, count the objects by using reduce method.
+
+//  EX:
+// var voters = [
+//     {
+//         voter_Name: "Adam Scott",
+//         votes_To: "James",
+//     },
+//     {
+//         voter_Name: "Abril Blake",
+//         votes_To: "Jade",
+//     },
+//     {
+//         voter_Name: "Ruby Andrews",
+//         votes_To: "Jade",
+//     },
+//     {
+//         voter_Name: "Junior Maxwell",
+//         votes_To: "Bailey",
+//     },
+//     {
+//         voter_Name: "Junior Maxwell",
+//         votes_To: "Bailey",
+//     }
+// ]
+//
+
+// ------------------------
+
+const objectCounter = (obj) => {
+    // write your code here
+    let count = 0;
+    obj.reduce((prev, current) => count++, 0)
+    return count;
+}
+
+// 2) ---------------------
+// 
+// Given a string input as an argument reverse it using reduce method.
+//
+//  EX:
+// 'you shall not pass' ==> 'pass not shall you'
+//
+// Hint:
+// since you need to reverse words, you may get some use of the methods
+// you learnt before to split the string to words
+//
+// ------------------------
+
+const stringReverse = (str) => {
+    let reverseArr = []
+    let arr = str.split(" ");
+    arr.reduce((prev, current, index) => reverseArr.unshift(current), 0)
+    let arrTostr = reverseArr.join(" ");
+    return arrTostr;
+}
+
+// 3) ---------------------
+// 
+// Using the same array of object from the first question, create new object that contain the candidates name as keys (voting for),
+// and how many times they got voted to as values using the reduce method.
+//
+// Input:
+// let voters = [
+//     {
+//         voter_Name: "Adam Scott",
+//         votes_To: "James",
+//     },
+//     {
+//         voter_Name: "Abril Blake",
+//         votes_To: "Jade",
+//     },
+//     {
+//         voter_Name: "Ruby Andrews",
+//         votes_To: "Jade",
+//     },
+//     {
+//         voter_Name: "Junior Maxwell",
+//         votes_To: "Bailey",
+//     },
+//     {
+//         voter_Name: "Junior Maxwell",
+//         votes_To: "Bailey",
+//     }
+// ];
+//
+// Output:
+// let res = {
+//     James: 1,
+//     Jade: 2,
+//     Bailey: 2
+// };
+//
+// ------------------------
+
+const statistics = (obj) => {
+    // write your code here
+    
+    let res = obj.reduce(function (prev,current) {
+        if (current.votes_To in prev){
+            prev[current.votes_To]++
+        }
+        else {
+            prev[current.votes_To]=1;
+        }
+        return prev;
+        console.log(prev);
+
+    },{})
+    return res;
+}
+
+
+
+module.exports = { objectCounter, stringReverse, statistics };
+
